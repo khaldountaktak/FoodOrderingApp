b00642e95a76ec3e11c0248461cc62aab2df76b6
Khaldoun TAKTAK <khaldoun.taktak@gmail.com>
2022-11-28 12:11:54 +0100

    Added the  Meals component containing a summary for the meals and the list of the availabale meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food, Delivered to you</h2>
+            <p>
+                Choose your favorite meal.
+            </p>
+        </section>
+    )
+}
+
+export default MealsSummary
