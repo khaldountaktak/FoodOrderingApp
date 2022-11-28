a04f2d3c56d15e4a51c2cfa753d7ad5f019670e0
Khaldoun TAKTAK <khaldoun.taktak@gmail.com>
2022-11-28 14:14:24 +0100

    Added state management for the Cart component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = props => {
         <>
             <header className={classes.header}>
                 <h1>FoodOrderingApp</h1>
-                <HeaderCartButton/>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
         <div className={classes["main-image"]}>
             <img src={mealsImage} alt='Meals ' />
