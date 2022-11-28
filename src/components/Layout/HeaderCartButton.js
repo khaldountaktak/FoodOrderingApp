26b1a5f5cc77c563a707f7ddbb5bbf159aa670be
Khaldoun TAKTAK <khaldoun.taktak@gmail.com>
2022-11-28 22:17:46 +0100

    Added cart adding and removing features

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,13 +2,24 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import { useContext } from "react";
-const HeaderCartButton = (props) => { 
-  const ctx = useContext(CartContext)
-  const numberOfCartItems= ctx.items.reduce((currNumber, item) => {
+import { useEffect,useState } from "react";
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted,setBtnIsHighlighted]= useState(false);
+  const ctx = useContext(CartContext);
+  const {items} = ctx;
+
+  const numberOfCartItems = ctx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
-  } , 0)
+  }, 0);
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  useEffect(() => {
+    if (ctx.items.length===0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
