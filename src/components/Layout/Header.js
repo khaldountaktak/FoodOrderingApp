12ad10677b62142ae8febd9e0351ba58312abfef
Khaldoun TAKTAK <khaldoun.taktak@gmail.com>
2022-11-28 11:57:11 +0100

    Added the cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import classes from './Header.module.css'
-
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg'
 const Header = props => {
     return(
         <>
             <header className={classes.header}>
                 <h1>FoodOrderingApp</h1>
-                <button>Cart</button>
+                <HeaderCartButton/>
             </header>
         <div className={classes["main-image"]}>
             <img src={mealsImage} alt='Meals ' />
