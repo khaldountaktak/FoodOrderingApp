5d91b485b5fe475f93bb0c14cadc25890152c31f
Khaldoun TAKTAK <khaldoun.taktak@gmail.com>
2022-11-28 12:50:49 +0100

    Added a MealItemForm which will handle adding food elements to the cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealtemForm';
 const MealItem = (props) => {
     const price= `$${props.price.toFixed(2)}`
   return (
@@ -9,7 +10,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+            <MealItemForm/>
       </div>
     </li>
   );
